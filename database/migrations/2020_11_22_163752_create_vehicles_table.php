c163a5cee438fae30dde751255ab018cdc0989a0
Christoffer johansen <christofferjohansen12@gmail.com>
2020-11-22 17:50:19 +0100

    Created migrations

---
+++ database/migrations/2020_11_22_163752_create_vehicles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('brand_id')->constrained('brands');
+            $table->foreignId('vehicle_model_id')->constrained('vehicle_models');
+            $table->year('year_model');
+            $table->integer('mileage');
+            $table->date('registration_date');
+            $table->boolean('veteran');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
